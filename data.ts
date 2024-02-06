fbec98171fdf43b95e2b5cafcadef4c42bf1e702
faznaa <faznaaharees@gmail.com>
2024-02-06 13:15:11 +0530

    comment

--- data.ts
+++ data.ts
@@ -3,7 +3,7 @@ import { FaServer } from "react-icons/fa";
 import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
 import { MdDeveloperMode } from "react-icons/md";
 import { IProject, Service, Skill } from "./types";
-
+//
 import { BsCircleFill } from "react-icons/bs";
 
 export const services: Service[] = [
